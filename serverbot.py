c2372d5b69683b5b10a75dc39330b26951c50ad7
めいびぃ <162483806+Maybe515@users.noreply.github.com>
2025-03-24 11:05:00 +0900

    Update serverbot.py

--- serverbot.py
+++ serverbot.py
@@ -3,48 +3,52 @@ import subprocess
 # インストールした discord.py を読み込む
 import discord
 
-# TOKENとサーバーの情報
-TOKEN = "ここに自分のbotのアクセストークンを入力"
-fi_name = "サーバー実行ファイル名.jar"
-maxMem = "8G"  # 任意の最大メモリ割り当てサイズ
-minMem = "8G"  # 任意の最小メモリ割り当てサイズ
+# Discordとbotの情報
+## TOKEN
+TOKEN = "botのアクセストークン"
 
-# Channel IDの情報
+## Channel ID
 chan_mc = ""    # Minecraft
 chan_ck = ""    # CoreKeeper
 chan_tr = ""    # Terraria
 chan_cmd = ""   # Commmand
+
+# サーバープロセスの情報
+## Minecraft
+mc_finm = "サーバー実行ファイル名.jar"
+maxMem = "8G"  # 任意の最大メモリ割り当てサイズ
+minMem = "8G"  # 任意の最小メモリ割り当てサイズ
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
 # サーバー操作用
 class MCserver_process:     # Minecraft
-    def __init__(self, fi_name, maxMem,minMem):
+    def __init__(self, mc_finm, maxMem,minMem):
         self.server = None
-        self.command = ["java", "-server",f"-Xms{minMem}", f"-Xmx{maxMem}", "-jar", fi_name, "nogui"]
+        self.command = ["java", "-server",f"-Xms{minMem}", f"-Xmx{maxMem}", "-jar", mc_finm, "nogui"]
     def start(self):
         self.server = subprocess.Popen(self.command, stdin=subprocess.PIPE)
     def stop(self):
         input_string = "stop"
         self.server.communicate(input_string.encode())
-MCserver = MCserver_process(fi_name, maxMem, minMem)
+MCserver = MCserver_process(mc_finm, maxMem, minMem)
 
 class CKserver_process:     # CoreKeeper
     def __init__(self, fi_name, maxMem,minMem):
         self.server = None
-        self.command = ["java", "-server",f"-Xms{minMem}", f"-Xmx{maxMem}", "-jar", fi_name, "nogui"]
+        self.command = ["batコマンドの処理"]
     def start(self):
         self.server = subprocess.Popen(self.command, stdin=subprocess.PIPE)
     def stop(self):
-        input_string = "Q"
+        input_string = "q"
         self.server.communicate(input_string.encode())
 CKserver = CKserver_process(fi_name, maxMem, minMem)
 
 class TRserver_process:     # Terraria
     def __init__(self, fi_name, maxMem,minMem):
         self.server = None
-        self.command = ["java", "-server",f"-Xms{minMem}", f"-Xmx{maxMem}", "-jar", fi_name, "nogui"]
+        self.command = ["batコマンドの処理"]
     def start(self):
         self.server = subprocess.Popen(self.command, stdin=subprocess.PIPE)
     def stop(self):
@@ -65,10 +69,6 @@ async def tr_rep(message):
     reply = f"{message.author.mention} TRgameID"
     await chan_cmd.send(reply)
 
-async def needRole_rep(message):
-    reply = f"{message.author.mention} このコマンドは権限が必要です"
-    await chan_cmd.send(reply)
-
 # 起動時に動作する処理
 @client.event
 async def on_ready():
@@ -81,50 +81,43 @@ async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
+        
+    # Minecraftサーバー
+    if message.content == "/mcstart":
+        await chan_mc.send("【Minecraft】サーバープロセスを実行します")
+        MCserver.start()
+    elif message.content == "/mcstop":
+        await chan_mc.send("【Minecraft】サーバープロセスを停止します")
+        MCserver.stop()
 
-    if message.content == "/"+*:
-        # 権限を持っているユーザーのみコマンド実行可能
-        if message.author.guild_permissions.administrator:
+    # CoreKeeperサーバー
+    if message.content == "/ckstart":
+        await chan_ck.send("【oreKeeper】サーバープロセスを実行します")
+        CKserver.start()
+    elif message.content == "/ckstop":
+        await chan_ck.send("【CoreKeeper】サーバープロセスを停止します")
+        CKserver.stop()
 
-            # Minecraftサーバー
-            if message.content == "/mcstart":
-                await chan_mc.send("【Minecraft】サーバープロセスを実行します")
-                MCserver.start()
-            elif message.content == "/mcstop":
-                await chan_mc.send("【Minecraft】サーバープロセスを停止します")
-                MCserver.stop()
+    # Terrariaサーバー
+    if message.content == "/trstart":
+        await chan_tr.send("【Terraria】サーバープロセスを実行します")
+        TRserver.start()
+    elif message.content == "/trstop":
+        await chan_tr.send("【Terraria】サーバープロセスを停止します")
+        TRserver.stop()
 
-            # CoreKeeperサーバー
-            if message.content == "/ckstart":
-                await chan_ck.send("【oreKeeper】サーバープロセスを実行します")
-                CKserver.start()
-            elif message.content == "/ckstop":
-                await chan_ck.send("【CoreKeeper】サーバープロセスを停止します")
-                CKserver.stop()
+    # GameID表示
+    if message.content == "/mcid":   # Minecraft
+        await mc_rep(message)
+    elif message.content == "/ckid": # CoreKeeper
+        await ck_rep(message)
+    elif message.content == "/trid": # Terraria
+        await tr_rep(message)        
 
-            # Terrariaサーバー
-            if message.content == "/trstart":
-                await chan_tr.send("【Terraria】サーバープロセスを実行します")
-                TRserver.start()
-            elif message.content == "/trstop":
-                await chan_tr.send("【Terraria】サーバープロセスを停止します")
-                TRserver.stop()
-
-            # GameID表示
-            if message.content == "/mcid"   # Minecraft
-                await mc_rep(message)
-            elif message.content == "/ckid" # CoreKeeper
-                await ck_rep(message)
-            elif message.content == "/trid" # Terraria
-                await tr_rep(message)        
-
-            # ヘルプ
-            if message.content == "/help":
-                await chan_cmd.send("ヘルプを表示します")
-                chan_cmd.send("")
-    else:
-        await needRole_rep(message) 
-        return
+    # ヘルプ
+    if message.content == "/help":
+        await chan_cmd.send("ヘルプを表示します")
+        chan_cmd.send("")
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
