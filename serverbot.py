5713844b2d1cbfd7605fc138338239853e6fba00
めいびぃ <162483806+Maybe515@users.noreply.github.com>
2025-04-01 10:27:31 +0900

    Update serverbot.py

--- serverbot.py
+++ serverbot.py
@@ -82,26 +82,26 @@ async def tr_rep(message):
 # Embedメッセージ
 async def mc_emb(message):    # Minecraft
     if state == "mc_join":
-        embed = discord.embed(title="Player Join", description="Joined player：" + player, color=0x57F287)
+        embed = discord.embed(title="Player Joined", description="Player：" + player, color=0x57F287)
         await chan_mc.send(embed=embed)
     elif state == "mc_left":
-        embed = discord.embed(title="Player Left", description="Lefted player：" + player, color=0xED4245)
+        embed = discord.embed(title="Player Left", description="Player：" + player, color=0xED4245)
         await chan_mc.send(embed=embed)
 
 async def ck_emb(message):    # CoreKeeper
     if state == "ck_join":
-        embed = discord.embed(title="Player Join", description="Joined player：" + player, color=0x57F287)
+        embed = discord.embed(title="Player Joined", description="Player：" + player, color=0x57F287)
         await chan_ck.send(embed=embed)
     elif state == "ck_left":
-        embed = discord.embed(title="Player Left", description="Lefted player：" + player, color=0xED4245)
+        embed = discord.embed(title="Player Left", description="Player：" + player, color=0xED4245)
         await chan_ck.send(embed=embed)
 
 async def tr_emb(message):    # Terraria
     if state == "tr_join":
-        embed = discord.embed(title="Player Join", description="Joined player：" + player, color=0x57F287)
+        embed = discord.embed(title="Player Joined", description="Player：" + player, color=0x57F287)
         await chan_tr.send(embed=embed)
     elif state == "tr_left":
-        embed = discord.embed(title="Player Left", description="Lefted player：" + player, color=0xED4245)
+        embed = discord.embed(title="Player Left", description="Player：" + player, color=0xED4245)
         await chan_tr.send(embed=embed)
 
 
@@ -132,7 +132,7 @@ async def on_message(message):
 
     # CoreKeeperサーバー
     if message.content == "/ckstart":
-        await chan_cmd.send("【oreKeeper】サーバープロセスを実行します")
+        await chan_cmd.send("【CoreKeeper】サーバープロセスを実行します")
         CKserver.start()
     elif message.content == "/ckstop":
         await chan_cmd.send("【CoreKeeper】サーバープロセスを停止します")
